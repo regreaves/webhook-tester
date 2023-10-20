2a258df284fc65968e7c98cef28d86499ab74fb4
Ralph Greaves <rgreaves@ycp.edu>
2023-10-20 19:44:45 -0400

    Add files via upload

---
+++ webhook_test.js
@@ -0,0 +1,41 @@
+const fastify = require('fastify')();
+
+const { fastifySchedulePlugin } = require('@fastify/schedule');
+const { SimpleIntervalJob, AsyncTask } = require('toad-scheduler');
+
+const task = new AsyncTask(
+    'simple task',
+    () => { return doFetch()
+      .then((result) => {
+        /* continue the promise chain */
+      }) },
+    (err) => {
+      /* handle errors here */
+    });
+
+const job = new SimpleIntervalJob({ seconds: 2, }, task)
+
+fastify.register(fastifySchedulePlugin);
+
+fastify.ready().then(() => {
+  fastify.scheduler.addSimpleIntervalJob(job);
+});
+
+async function doFetch() {
+  const request = new Request(
+    process.env.WEBHOOK_URL,
+    {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({ content: 'hello, world' })
+    }
+  );
+
+  await fetch(request);
+}
+
+
+
+
+
+
